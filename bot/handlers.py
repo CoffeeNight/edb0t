0ea242aa04aff036e2f36db5fa22306a31724c6c
Edgar Contreras <edgar@nochesdecafeina.com>
2017-02-27 21:43:55 +0100

    - The static method didn't work. Rolled back to regular methods and added the init_handlers method. - Fixed welcome text formatting

--- bot/handlers.py
+++ bot/handlers.py
@@ -15,33 +15,32 @@ class Handlers(object):
         self.dispatcher = dispatcher
 
     def init_handlers(self):
-        self.dispatcher.add_handler(self._new_member_handler)
+        new_member_handler = MessageHandler(
+            NewChatMemberFilter(), self.new_member)
+        self.dispatcher.add_handler(new_member_handler)
 
-    @staticmethod
-    def new_member(bot, update):
+    def new_member(self, bot, update):
+        username = update.message.new_chat_member.username
         welcome_text = """
-        {first_name} {last_name} ({username}), bienvenido(a) al grupo Telegram de Seguridad Informática Venezuela: https://t.me/itsec_ve
+Bienvenido(a) {username} al grupo Telegram de Seguridad Informática Venezuela: https://t.me/itsec_ve
 
-        Compartimos ideas relacionadas con el tema de seguridad informática.
-        Criptografía, privacidad, PenTest, exploits, ingeniería reversa, entre otros.
+Compartimos ideas relacionadas con el tema de seguridad informática.
+Criptografía, privacidad, PenTest, exploits, ingeniería reversa, entre otros.
 
-        Algunos consejos y normas para empezar:
+Algunos consejos y normas para empezar:
 
-        * Preséntate cuando llegues (Chévere si nos cuentas que interés tienes en la Seguridad Informática)
-        * Temas on-topic: recursos, herramientas, experiencias, técnicas, etc.
-        * Las discusiones en el grupo se rigen por el código de conducta "convenido para contribuyentes" http://contributor-covenant.org/version/1/4/es/code_of_conduct.txt
-        * En concreto, las bromas de contenido sexual o inapropiado no están permitidas.
-        * No apoyamos el uso de estas herramientas y conocimientos para fines que estén por fuera del marco de la ley.
-        * Este grupo está moderado, sé respetuoso y tolerante con los demás
-        * "We are all consenting adults here"
-        """.format({
-            'first_name': update.message.new_chat_member.first_name,
-            'last_name': update.message.new_chat_member.last_name,
-            'username': update.message.new_chat_member.username
-        })
+* Preséntate cuando llegues (Chévere si nos cuentas que interés tienes en la Seguridad Informática)
+* Temas on-topic: recursos, herramientas, experiencias, técnicas, etc.
+* Las discusiones en el grupo se rigen por el código de conducta "convenido para contribuyentes" http://contributor-covenant.org/version/1/4/es/code_of_conduct.txt
+* En concreto, las bromas de contenido sexual o inapropiado no están permitidas.
+* No apoyamos el uso de estas herramientas y conocimientos para fines que estén por fuera del marco de la ley.
+* Este grupo está moderado, sé respetuoso y tolerante con los demás
+* "We are all consenting adults here"
+        """.format(
+            username='@' + username if username != '' else '',
+        )
 
         bot.sendMessage(
             chat_id=update.message.chat_id,
             text=welcome_text
         )
-    _new_member_handler = MessageHandler(NewChatMemberFilter(), new_member)
