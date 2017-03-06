2fcd3b2d0a86ea140cf1199eaa1e0e2fd65625f7
Edgar Contreras <edgar@nochesdecafeina.com>
2017-03-06 16:22:21 +0100

    Simplified the welcome message

--- bot/handlers.py
+++ bot/handlers.py
@@ -21,22 +21,7 @@ class Handlers(object):
 
     def new_member(self, bot, update):
         username = update.message.new_chat_member.username
-        welcome_text = """
-Bienvenido(a) {username} al grupo Telegram de Seguridad Informática Venezuela: https://t.me/itsec_ve
-
-Compartimos ideas relacionadas con el tema de seguridad informática.
-Criptografía, privacidad, PenTest, exploits, ingeniería reversa, entre otros.
-
-Algunos consejos y normas para empezar:
-
-* Preséntate cuando llegues (Chévere si nos cuentas que interés tienes en la Seguridad Informática)
-* Temas on-topic: recursos, herramientas, experiencias, técnicas, etc.
-* Las discusiones en el grupo se rigen por el código de conducta "convenido para contribuyentes" http://contributor-covenant.org/version/1/4/es/code_of_conduct.txt
-* En concreto, las bromas de contenido sexual o inapropiado no están permitidas.
-* No apoyamos el uso de estas herramientas y conocimientos para fines que estén por fuera del marco de la ley.
-* Este grupo está moderado, sé respetuoso y tolerante con los demás
-* "We are all consenting adults here"
-        """.format(
+        welcome_text = 'Hola {username}!'.format(
             username='@' + username if username != '' else '',
         )
 
